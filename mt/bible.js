823b79eb8364725c29d77aad421715fad2b8b009
RENPWN <46776762+renpwn@users.noreply.github.com>
2025-12-27 03:45:43 +0700

    update toSuperscript

--- mt/bible.js
+++ mt/bible.js
@@ -487,12 +487,10 @@ async function buildSabdaUrl(bookId, chapter, versions = BibleVersions) {
    SUPERSCRIPT
 ========================= */
 function toSuperscript(num) {
-  const map = {
-    '0':'⁰','1':'¹','2':'²','3':'³','4':'⁴',
-    '5':'⁵','6':'⁶','7':'⁷','8':'⁸','9':'⁹'
-  }
-  return num.split('').map(n => map[n] || n).join('')
+  const sup = '⁰¹²³⁴⁵⁶⁷⁸⁹'
+  return '⁽' + String(num).split('').map(n => sup[n]).join('') + '⁾'
 }
+
 
 async function parseChapterHTMLori(html, bookId, chapter, targetVersions) {
   const $ = cheerio.load(html);
